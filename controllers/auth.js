094f5af0941dae039742b933cad9057420205e61
Chii1914 <133626251+Chii1914@users.noreply.github.com>
2023-09-26 01:07:02 -0300

    Back funcionando

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,17 +4,17 @@ import bcrypt from "bcryptjs"
 export const register = (req, res)=>{
     //Mirar existencias
     const q = "SELECT * FROM usuario WHERE RUN = ?"
+
     db.query(q, [req.body.RUN], (err, data) =>{
         if(err) return res.json(err)
         if(data.lenght) return res.status(409).json("Usuario YA registrado")
-
         //Hash the password and create user
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(req.body.password, salt)
-
+        
         const q = "INSERT INTO usuario (RUN, password) VALUES (?)"
         const values = [
-            req.body.RUN,
+            req.body.username,
             hash,
         ]
 
