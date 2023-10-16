700cff9c11cced47fa920d998b225a475c128e57
tanio-tiano <lucciano.caceres@alumnos.uv.cl>
2023-10-16 07:22:52 -0300

    BOLETA - USER: CRUD CORREGIDO

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,3 @@
-
 import mysql2 from "mysql2/promise";
 import connectionConfig from "../database/connection.js";
 import bcrypt from "bcryptjs";
@@ -10,7 +9,7 @@ import bcrypt from "bcryptjs";
 const createConnection = async () => {
   return await mysql2.createConnection(connectionConfig);
 };
-//CREAR EL CRUD PA EVENTOS Y ACTUALIZAR DELETE BIEM 
+//CREAR EL CRUD PA EVENTOS Y ACTUALIZAR DELETE BIEM
 /**
  * This JavaScript function creates a user by inserting their name and email into a database table.
  * @param req - The `req` parameter is the request object that contains information about the incoming
@@ -25,11 +24,20 @@ const crearUsuario = async (req, res) => {
   try {
     const connection = await createConnection();
     const usuario = req.body;
-    const saltRounds = 10
+    const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(usuario.password, saltRounds);
     await connection.execute(
       "INSERT INTO usuario (RUN, password, Dirección_completa, telefono_emergencia, nombre_completo, rol, categoria, telefono) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-      [usuario.RUN, hashedPassword, usuario.Dirección_completa, usuario.telefono_emergencia, usuario.nombre_completo,usuario.rol, usuario.categoria, usuario.telefono]
+      [
+        usuario.RUN,
+        hashedPassword,
+        usuario.Dirección_completa,
+        usuario.telefono_emergencia,
+        usuario.nombre_completo,
+        usuario.rol,
+        usuario.categoria,
+        usuario.telefono,
+      ]
     );
     await connection.end();
     return res.status(200).json({
@@ -98,60 +106,62 @@ const getUsuarioID = async (req, res) => {
   }
 };
 
-const actualizarID = async (req, res) =>{
-    try{
-        const connection = await createConnection();
-        const usuario = req.body;
-        const RUN = req.params;
-        const saltRounds = 10
-        const hashedPassword = await bcrypt.hash(usuario.password, saltRounds);
-        await connection.execute(
-        "UPDATE usuario SET Dirección_completa = ?, telefono_emergencia = ?, nombre_completo = ?, rol = ?, categoria = ?, telefono = ?, password = ? WHERE RUN = ?",
-        [usuario.Dirección_completa,
+const actualizarID = async (req, res) => {
+  try {
+    const connection = await createConnection();
+    const usuario = req.body;
+    const RUN = req.params;
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(usuario.password, saltRounds);
+    await connection.execute(
+      "UPDATE usuario SET Dirección_completa = ?, telefono_emergencia = ?, nombre_completo = ?, rol = ?, categoria = ?, telefono = ?, password = ? WHERE RUN = ?",
+      [
+        usuario.Dirección_completa,
         usuario.telefono_emergencia,
         usuario.nombre_completo,
         usuario.rol,
         usuario.categoria,
         usuario.telefono,
         hashedPassword,
-        RUN]
-        );
-        await connection.end();
-        return res.status(200).json({
-        status: true,
-        usuarios: rows,
-        });
-    }catch(error){
-        return res.status(500).json({
-            status: false,
-            error: "Problemas al actualizar el usuario o no existe",
-            code: error,
-          });
-    }
-}
+        RUN.RUN,
+      ]
+    );
+    await connection.end();
+    return res.status(200).json({
+      status: true,
+      message: "El usuario fue actualizado",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      error: "Problemas al actualizar el usuario o no existe",
+      code: error,
+    });
+  }
+};
 
-const deleteID = async (req, res) =>{
-    try{
-        const connection = await createConnection();
-        const usuario = req.params;
-        const {RUN} = usuario;
-        await connection.execute('DELETE FROM usuario WHERE RUN = ?', [RUN]);
-        await connection.end();
-        return res.status(200).json({
-            status: true,
-            message: 'Usuario eliminado',
-            });
-    }catch(error){
-        return res.status(500).json({
-            status: false,
-            error: "Problemas al eliminar el usuario o no existe",
-            code: error,
-          });
-    }
-}
+const deleteID = async (req, res) => {
+  try {
+    const connection = await createConnection();
+    const usuario = req.params;
+    const { RUN } = usuario;
+    await connection.execute("DELETE FROM usuario WHERE RUN = ?", [RUN]);
+    await connection.end();
+    return res.status(200).json({
+      status: true,
+      message: "Usuario eliminado",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      error: "Problemas al eliminar el usuario o no existe",
+      code: error,
+    });
+  }
+};
 
 /* The `export` statement is used to export functions, objects, or values from a module so that they
 can be imported and used in other modules. In this case, the `export` statement is exporting the
 `getUsuarios` and `crearUsuario` functions from the current module. This allows other modules to
 import and use these functions. */
-export { getUsuarios, crearUsuario, getUsuarioID, actualizarID, deleteID};
+export { getUsuarios, crearUsuario, getUsuarioID, actualizarID, deleteID };
