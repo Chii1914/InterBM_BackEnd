d572e5a19f8ce0562899abcf13a95514a76a73bc
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2023-10-16 06:37:42 -0300

    Resolución problemas merge

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,5 @@
 /* The code is creating a router object using the `Router` class from the `express` module. It then
 defines two routes: */
-<<<<<<< HEAD
-import { Router } from 'express';
-
-import { crearUsuario, getUsuarios } from '../controllers/user.js';
-=======
 import { Router } from "express";
 
 import {
@@ -14,27 +9,11 @@ import {
   getUsuarioID,
   getUsuarios,
 } from "../controllers/user.js";
->>>>>>> Chii-Branch
 
 /* `const router = Router();` is creating a new router object using the `Router` class from the
 `express` module. This router object will be used to define routes and handle HTTP requests. */
 const router = Router();
 
-<<<<<<< HEAD
-/* `router.route('/create').post(crearUsuario)` is defining a route for the HTTP POST method on the
-'/create' endpoint. */
-router.route('/create')
-    .post(crearUsuario);
-
-/* `router.route('/get-all').get(getUsuarios)` is defining a route for the HTTP GET method on the
-'/get-all' endpoint. This means that when a GET request is made to the '/get-all' endpoint, the
-`getUsuarios` function will be called to handle the request. */
-router.route('/get-all')
-    .get( getUsuarios );
-
-
-export default router;
-=======
 
 router.route("/user/").post(crearUsuario);
 router.route("/user/").get(getUsuarios);
@@ -45,4 +24,3 @@ router.route("/user/:RUN").delete(deleteID)
 
 
 export default router;
->>>>>>> Chii-Branch
