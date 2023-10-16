ab7b3a9af77ebc7ac6ea26147420370f9f868c45
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2023-10-16 06:27:41 -0300

    Revert "Merge branch 'Chii-Branch'"
    This reverts commit 938994dbe5fc7d5433707f606267a5ca51676f39, reversing
    changes made to 9ffacca85617de48372071c1ba0738d000f39b3c.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import mysql2 from "mysql2/promise";
 import connectionConfig from "../database/connection.js";
 import bcrypt from "bcryptjs";
@@ -154,3 +155,5 @@ can be imported and used in other modules. In this case, the `export` statement 
 `getUsuarios` and `crearUsuario` functions from the current module. This allows other modules to
 import and use these functions. */
 export { getUsuarios, crearUsuario, getUsuarioID, actualizarID, deleteID};
+=======
+>>>>>>> parent of 938994d (Merge branch 'Chii-Branch')
