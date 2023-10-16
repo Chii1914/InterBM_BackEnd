7d1160e784a36d0d1732e841f599788774d9e9d4
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2023-10-16 05:53:24 -0300

    Se hace el crud de user, post, get, delete y patch

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,14 +1,14 @@
-import mysql2 from 'mysql2/promise';
-import connectionConfig from '../database/connection.js';
-import bcrypt from "bcryptjs"
+import mysql2 from "mysql2/promise";
+import connectionConfig from "../database/connection.js";
+import bcrypt from "bcryptjs";
 
 /**
  * The function creates a connection to a MySQL database using the provided configuration.
  * @returns a promise that resolves to a MySQL connection object.
  */
-const createConnection = async ( ) => {
-    return await mysql2.createConnection(connectionConfig);
-}
+const createConnection = async () => {
+  return await mysql2.createConnection(connectionConfig);
+};
 
 /**
  * This JavaScript function creates a user by inserting their name and email into a database table.
@@ -21,40 +21,27 @@ const createConnection = async ( ) => {
  * created successfully.
  */
 const crearUsuario = async (req, res) => {
-    try {
-        //const usuario = req.body; //en el cuerpo de la solicitud vamos a enviar data  'POST PUT'
-        // -const filters = req.query; //?query1=valorquery1&query2=valorquery2
-        // const parametros = req.params; // /create/:parametro
-        const connection = await createConnection();
-        const q = 'SELECT * FROM usuario where RUN = ?'
-        console.log(req.body.username + " FLAGGG " + req.body.password)
-        /* 
-        //SI SE DESCOMENTA ESTA WEA QUEDA LA CAGA NO SÉ PORQUEEE
-        await connection.query(q, [req.body.username], (err, data) =>{
-            if(err) return res.json(err)
-            if(data.length) return res.status(409).json("Usuario YA registrado") 
-        })
-        */
-        
-        await connection.execute('INSERT INTO usuario (RUN, password) VALUES (?, ?)', [req.body.username, req.body.password]);
-        await connection.end();
-       
-        // console.log("BODY", usuario);
-        // console.log("QUERY", filters);
-        // console.log("PARAMS", parametros);
-
-        return res.status(200).json({
-            status: true,
-            message: "Usuario creado"
-        });
-        
-    } catch (error) {
-        return res.status(500).json({
-            status: false,
-            error: "Problemas al crear el usuario",
-            code: error
-        });
-    };
+  try {
+    const connection = await createConnection();
+    const usuario = req.body;
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(usuario.password, saltRounds);
+    await connection.execute(
+      "INSERT INTO usuario (RUN, password, Dirección_completa, telefono_emergencia, nombre_completo, rol, categoria, telefono) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      [usuario.RUN, hashedPassword, usuario.Dirección_completa, usuario.telefono_emergencia, usuario.nombre_completo,usuario.rol, usuario.categoria, usuario.telefono]
+    );
+    await connection.end();
+    return res.status(200).json({
+      status: true,
+      message: "Usuario creado",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      error: "Problemas al crear el usuario o usuario ya está registrado",
+      code: error,
+    });
+  }
 };
 
 /**
@@ -70,25 +57,95 @@ const crearUsuario = async (req, res) => {
  * @returns a response with a status code of 200 and a JSON object containing the success status and
  * the list of users (rows) retrieved from the database.
  */
-const getUsuarios = async ( req, res ) => {
-    try {
-        
+const getUsuarios = async (req, res) => {
+  try {
+    const connection = await createConnection();
+    const [rows] = await connection.execute("SELECT * from usuario");
+    await connection.end();
+    return res.status(200).json({
+      success: true,
+      usuarios: rows,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      error: "Problemas al traer el usuarios",
+      code: error,
+    });
+  }
+};
+
+const getUsuarioID = async (req, res) => {
+  try {
+    const connection = await createConnection();
+    const usuario = req.params;
+    const [rows] = await connection.execute(
+      "SELECT * FROM usuario WHERE RUN = ?",
+      [usuario.RUN]
+    );
+    await connection.end();
+    return res.status(200).json({
+      status: true,
+      usuarios: rows,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      error: "Problemas al visualizar usuario o no existe",
+      code: error,
+    });
+  }
+};
+
+const actualizarID = async (req, res) =>{
+    try{
         const connection = await createConnection();
-        console.log("JSAH")
-        const [rows] = await connection.execute('SELECT * from usuario');
+        const usuario = req.body;
+        const RUN = req.params;
+        const saltRounds = 10
+        const hashedPassword = await bcrypt.hash(usuario.password, saltRounds);
+        await connection.execute(
+        "UPDATE usuario SET Dirección_completa = ?,, password = ? WHERE RUN = ?",
+        [usuario.Dirección_completa,
+        usuario.telefono_emergencia,
+        usuario.nombre_completo,
+        usuario.rol,
+        usuario.categoria,
+        usuario.telefono,
+        hashedPassword,
+        RUN]
+        );
         await connection.end();
-
         return res.status(200).json({
-            success: true,
-            usuarios: rows
+        status: true,
+        usuarios: rows,
         });
-
-    } catch (error) {
+    }catch(error){
         return res.status(500).json({
             status: false,
-            error: "Problemas al traer el usuarios",
-            code: error
-        });
+            error: "Problemas al actualizar el usuario o no existe",
+            code: error,
+          });
+    }
+}
+
+const deleteID = async (req, res) =>{
+    try{
+        const connection = await createConnection();
+        const usuario = req.params;
+        const {RUN} = usuario;
+        await connection.execute('DELETE FROM usuario WHERE RUN = ?', [RUN]);
+        await connection.end();
+        return res.status(200).json({
+            status: true,
+            message: 'Usuario eliminado',
+            });
+    }catch(error){
+        return res.status(500).json({
+            status: false,
+            error: "Problemas al eliminar el usuario o no existe",
+            code: error,
+          });
     }
 }
 
@@ -96,7 +153,4 @@ const getUsuarios = async ( req, res ) => {
 can be imported and used in other modules. In this case, the `export` statement is exporting the
 `getUsuarios` and `crearUsuario` functions from the current module. This allows other modules to
 import and use these functions. */
-export {
-    getUsuarios,
-    crearUsuario,
-}
+export { getUsuarios, crearUsuario, getUsuarioID, actualizarID, deleteID};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,23 +1,26 @@
 /* The code is creating a router object using the `Router` class from the `express` module. It then
 defines two routes: */
-import { Router } from 'express';
+import { Router } from "express";
 
-import { crearUsuario, getUsuarios } from '../controllers/user.js';
+import {
+    actualizarID,
+  crearUsuario,
+  deleteID,
+  getUsuarioID,
+  getUsuarios,
+} from "../controllers/user.js";
 
 /* `const router = Router();` is creating a new router object using the `Router` class from the
 `express` module. This router object will be used to define routes and handle HTTP requests. */
 const router = Router();
 
-/* `router.route('/create').post(crearUsuario)` is defining a route for the HTTP POST method on the
-'/create' endpoint. */
-router.route('/create')
-    .post(crearUsuario);
 
-/* `router.route('/get-all').get(getUsuarios)` is defining a route for the HTTP GET method on the
-'/get-all' endpoint. This means that when a GET request is made to the '/get-all' endpoint, the
-`getUsuarios` function will be called to handle the request. */
-router.route('/get-all')
-    .get( getUsuarios );
+router.route("/user/").post(crearUsuario);
+router.route("/user/").get(getUsuarios);
+router.route("/user/:RUN").get(getUsuarioID);
+router.route("/user/:RUN").patch(actualizarID)
+router.route("/user/:RUN").delete(deleteID)
+
 
 
 export default router;
