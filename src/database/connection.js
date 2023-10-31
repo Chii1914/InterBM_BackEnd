ce3e4a2acf6f53e5b7e88f3283f6fef389c7571d
stephano <stephano.almendra@alumnos.uv.cl>
2023-10-31 03:36:26 -0300

    se agrega el proyecto

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,25 +1,20 @@
-import mysql2 from "mysql2";
+import mysql from "mysql2";
 import values from "../const/const.js";
 
-/* The `connectionConfig` object is storing the configuration details for connecting to a MySQL
-database. It includes the following properties: */
 const connectionConfig = {
   host: values.HOST,
   user: values.USER,
   password: "",
   database: values.DATABASE,
 };
-/* The code is creating a connection to a MySQL database using the `mysql2` library. */
 
-const connection = mysql2.createConnection({
+const tempConnection = mysql.createConnection({
   host: connectionConfig.host,
   user: connectionConfig.user,
   password: connectionConfig.password,
 });
 
-/* The code `connection.query(`CREATE DATABASE IF NOT EXISTS ${connectionConfig.database}`, (error) =>
-{ ... })` is executing a SQL query to create a database if it does not already exist. */
-connection.query(
+tempConnection.query(
   `CREATE DATABASE IF NOT EXISTS ${connectionConfig.database}`,
   (error) => {
     if (error) {
@@ -29,12 +24,12 @@ connection.query(
 
     console.log("Base de datos creada o ya existente");
 
-    // Conectar a la base de datos
-    connectionConfig.database = connectionConfig.database;
-
     // Cerrar la conexión temporal
-    connection.end();
+    tempConnection.end();
   }
 );
 
-export default connectionConfig;
+// Crear la conexión real a la base de datos
+const db = mysql.createConnection(connectionConfig).promise();
+
+export default db;
--- src/index.js
+++ src/index.js
@@ -10,9 +10,8 @@ import value from './const/const.js';
 incoming requests on a specified port. The port number is determined by the value of
 `value.RUN_PORT` or defaults to 5000 if `value.RUN_PORT` is undefined. */
 const main = (() => {
-    const server = app.listen( value.RUN_PORT || 5000 ); //instancia 
-    console.log("Server activo", value.RUN_PORT || 5000);
+    const server = app.listen( value.RUN_PORT || 4000 ); //instancia 
+    console.log("Server activo", value.RUN_PORT || 4000);
     server.timeout = 600000;
 })();
 
-// jeje
